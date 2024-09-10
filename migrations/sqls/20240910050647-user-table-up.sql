7d69e140941037d15804e1f2c7592f99000eb469
rlondon3 <codesmithr@outlook.com>
2024-09-09 23:27:52 -0700

    chore: config db-migrate for user table

---
+++ migrations/sqls/20240910050647-user-table-up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        firstname VARCHAR(255) NOT NULL,
+        lastname VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        martial_art VARCHAR(255),
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        isAdmin BOOLEAN DEFAULT FALSE
+        );
